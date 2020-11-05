22d5c0455230761f678306ea9d93908516e7bb7d
sunx <1059363093@qq.com>
2020-11-05 22:48:52 +0800

    ..

---
+++ cifar_eval.py
@@ -0,0 +1,187 @@
+import argparse
+import os
+import random
+import time
+import warnings
+import sys
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.parallel
+import torch.backends.cudnn as cudnn
+import torch.optim
+import torch.multiprocessing as mp
+import torch.utils.data
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+import models
+from tensorboardX import SummaryWriter
+from sklearn.metrics import confusion_matrix
+from utils import *
+from imbalance_cifar import IMBALANCECIFAR10, IMBALANCECIFAR100
+from losses import LDAMLoss, FocalLoss
+
+model_names = sorted(name for name in models.__dict__
+    if name.islower() and not name.startswith("__")
+    and callable(models.__dict__[name]))
+
+parser = argparse.ArgumentParser(description='PyTorch Cifar Training')
+parser.add_argument('--dataset', default='cifar10', help='dataset setting')
+parser.add_argument('-a', '--arch', metavar='ARCH', default='resnet32',
+                    choices=model_names,
+                    help='model architecture: ' +
+                        ' | '.join(model_names) +
+                        ' (default: resnet32)')
+parser.add_argument('--loss_type', default="CE", type=str, help='loss type')
+parser.add_argument('--imb_type', default="exp", type=str, help='imbalance type')
+parser.add_argument('--imb_factor', default=0.01, type=float, help='imbalance factor')
+parser.add_argument('--train_rule', default='None', type=str, help='data sampling strategy for train loader')
+parser.add_argument('--rand_number', default=0, type=int, help='fix random number for data sampling')
+parser.add_argument('--exp_str', default='0', type=str, help='number to indicate which experiment it is')
+parser.add_argument('-j', '--workers', default=4, type=int, metavar='N',
+                    help='number of data loading workers (default: 4)')
+parser.add_argument('--epochs', default=200, type=int, metavar='N',
+                    help='number of total epochs to run')
+parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
+                    help='manual epoch number (useful on restarts)')
+parser.add_argument('-b', '--batch-size', default=128, type=int,
+                    metavar='N',
+                    help='mini-batch size')
+parser.add_argument('--lr', '--learning-rate', default=0.1, type=float,
+                    metavar='LR', help='initial learning rate', dest='lr')
+parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
+                    help='momentum')
+parser.add_argument('--wd', '--weight-decay', default=2e-4, type=float,
+                    metavar='W', help='weight decay (default: 1e-4)',
+                    dest='weight_decay')
+parser.add_argument('-p', '--print-freq', default=10, type=int,
+                    metavar='N', help='print frequency (default: 10)')
+parser.add_argument('--resume', default='', type=str, metavar='PATH',
+                    help='path to latest checkpoint (default: none)')
+parser.add_argument('-e', '--evaluate', dest='evaluate', action='store_true',
+                    help='evaluate model on validation set')
+parser.add_argument('--pretrained', dest='pretrained', action='store_true',
+                    help='use pre-trained model')
+parser.add_argument('--seed', default=None, type=int,
+                    help='seed for initializing training. ')
+parser.add_argument('--gpu', default='0', type=int,
+                    help='GPU id to use.')
+parser.add_argument('--root_log',type=str, default='log')
+parser.add_argument('--root_model', type=str, default='checkpoint')
+best_acc1 = 0
+
+
+def main():
+    args = parser.parse_args()
+    args.store_name = '_'.join([args.dataset, args.arch, args.loss_type, args.train_rule, args.imb_type, str(args.imb_factor), args.exp_str])
+    prepare_folders(args)
+    if args.seed is not None:
+        random.seed(args.seed)
+        torch.manual_seed(args.seed)
+        cudnn.deterministic = True
+        warnings.warn('You have chosen to seed training. '
+                      'This will turn on the CUDNN deterministic setting, '
+                      'which can slow down your training considerably! '
+                      'You may see unexpected behavior when restarting '
+                      'from checkpoints.')
+
+    if args.gpu is not None:
+        warnings.warn('You have chosen a specific GPU. This will completely '
+                      'disable data parallelism.')
+
+    ngpus_per_node = torch.cuda.device_count()
+    main_worker(args.gpu, ngpus_per_node, args)
+
+
+def main_worker(gpu, ngpus_per_node, args):
+    global best_acc1
+    args.gpu = gpu
+
+    if args.gpu is not None:
+        print("Use GPU: {} for evaluating".format(args.gpu))
+
+    # create model
+    print("=> creating model '{}'".format(args.arch))
+    num_classes = 100 if args.dataset == 'cifar100' else 10
+    use_norm = True if args.loss_type == 'LDAM' else False
+    model = models.__dict__[args.arch](num_classes=num_classes, use_norm=use_norm)
+    load_best_checkpoint(args, model)
+
+    if args.gpu is not None:
+        torch.cuda.set_device(args.gpu)
+        model = model.cuda(args.gpu)
+    else:
+        # DataParallel will divide and allocate batch_size to all available GPUs
+        model = torch.nn.DataParallel(model).cuda()
+
+    cudnn.benchmark = True
+
+    # Data loading code
+
+    transform_val = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
+
+    if args.dataset == 'cifar10':
+        val_dataset = datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_val)
+    elif args.dataset == 'cifar100':
+        val_dataset = datasets.CIFAR100(root='./data', train=False, download=True, transform=transform_val)
+    else:
+        warnings.warn('Dataset is not listed')
+        return
+
+
+    # evaluate on validation set
+    val_loader = torch.utils.data.DataLoader(
+        val_dataset, batch_size=100, shuffle=False,
+        num_workers=args.workers, pin_memory=True)
+    validate(val_loader, model, args)
+
+
+def validate(val_loader, model, args, flag='val'):
+    batch_time = AverageMeter('Time', ':6.3f')
+    top1 = AverageMeter('Acc@1', ':6.2f')
+    top5 = AverageMeter('Acc@5', ':6.2f')
+    
+    # switch to evaluate mode
+    model.eval()
+    all_preds = []
+    all_targets = []
+    with torch.no_grad():
+        end = time.time()
+        for i, (input, target) in enumerate(val_loader):
+            if args.gpu is not None:
+                input = input.cuda(args.gpu, non_blocking=True)
+            target = target.cuda(args.gpu, non_blocking=True)
+
+            # compute output
+            output = model(input)
+
+            # measure accuracy and record loss
+            acc1, acc5 = accuracy(output, target, topk=(1, 5))
+            top1.update(acc1[0], input.size(0))
+            top5.update(acc5[0], input.size(0))
+
+            # measure elapsed time
+            batch_time.update(time.time() - end)
+            end = time.time()
+
+            _, pred = torch.max(output, 1)
+            all_preds.extend(pred.cpu().numpy())
+            all_targets.extend(target.cpu().numpy())
+
+        cf = confusion_matrix(all_targets, all_preds).astype(float)
+        cls_cnt = cf.sum(axis=1)
+        cls_hit = np.diag(cf)
+        cls_acc = cls_hit / cls_cnt
+        output = ('{flag} Results: Prec@1 {top1.avg:.3f} Prec@5 {top5.avg:.3f}'
+                .format(flag=flag, top1=top1, top5=top5))
+        out_cls_acc = '%s Class Accuracy: %s'%(flag,(np.array2string(cls_acc, separator=',', formatter={'float_kind':lambda x: "%.3f" % x})))
+        print(output)
+        print(out_cls_acc)
+    return top1.avg
+
+
+if __name__ == '__main__':
+    main()
